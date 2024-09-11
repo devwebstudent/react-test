a08d0cc46a08d1aabb0ab8841e4157bc3f820f73
devwebstudent <142200863+devwebstudent@users.noreply.github.com>
2024-09-11 08:34:20 -0300

    atualizando códigos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
       <h1>Em construção.</h1>
       <p>Volte mais tarde!</p>
+
+      <h2>Apenas para testar useState</h2>
+      <p>Clicou: {count}</p>
+      <button onClick={(e) => setCount(count + 1)}>Clique aqui</button>
     </div>
   )
 }
