2f2c296d0a5e73c78bec9d1bb1ba1096be4e8c10
devwebstudent <142200863+devwebstudent@users.noreply.github.com>
2024-09-15 12:13:14 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,33 @@
-import './App.css'
+import "./App.css";
 
 // Ícones
 import { MdOutlineAdsClick } from "react-icons/md";
 import { RxReset } from "react-icons/rx";
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 function App() {
+    const [count, setCount] = useState(0);
 
-  const [count, setCount] = useState(0)
+    return (
+        <div className="App">
+            <h1>Em construção.</h1>
+            <p>Volte mais tarde!</p>
 
-  return (
-    <div className='App'>
-      <h1>Em construção.</h1>
-      <p>Volte mais tarde!</p>
+            <h2>Apenas para testar useState</h2>
+            <p>Clicou: {count}</p>
 
-      <h2>Apenas para testar useState</h2>
-      <p>
-        Clicou: {count}
-      </p>
+            <div className="control">
+                <button onClick={(e) => setCount(count + 1)}>
+                    <MdOutlineAdsClick className="click" />
+                </button>
 
-      <div className="control">
-        <button onClick={(e) => setCount(count + 1)}>          
-          <MdOutlineAdsClick className='click'/>
-        </button>
-
-        <button onClick={(e) => setCount(0)}>          
-          <RxReset className='reset'/>
-        </button>
-      </div>
-    </div>
-  )
+                <button onClick={(e) => setCount(0)}>
+                    <RxReset className="reset" />
+                </button>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
