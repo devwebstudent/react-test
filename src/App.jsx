0bd20a33ea1bb106f3e77e762ec6cc339b882123
devwebstudent <142200863+devwebstudent@users.noreply.github.com>
2024-09-15 12:43:02 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 // Ícones
 import { MdOutlineAdsClick } from "react-icons/md";
 import { RxReset } from "react-icons/rx";
-
-import { useState, useEffect } from "react";
+import { FaGithub } from "react-icons/fa";
+import { FaSteam } from "react-icons/fa";
 
 function App() {
     const [count, setCount] = useState(0);
@@ -12,19 +13,27 @@ function App() {
     return (
         <div className="App">
             <h1>Em construção.</h1>
-            <p>Volte mais tarde!</p>
 
-            <h2>Apenas para testar useState</h2>
-            <p>Clicou: {count}</p>
+            <div className="state">
+                <h2>Apenas para testar useState</h2>
+                <p>Clicou: {count}</p>
 
-            <div className="control">
-                <button onClick={(e) => setCount(count + 1)}>
-                    <MdOutlineAdsClick className="click" />
-                </button>
+                <div className="control">
+                    <button onClick={(e) => setCount(count + 1)}>
+                        <MdOutlineAdsClick className="click" />
+                    </button>
+                    <button onClick={(e) => setCount(0)}>
+                        <RxReset className="reset" />
+                    </button>
+                </div>
+            </div>
 
-                <button onClick={(e) => setCount(0)}>
-                    <RxReset className="reset" />
-                </button>
+            <h3>Onde me encontrar:</h3>
+            <div className="socials">
+                <ul>
+                    <li><FaGithub /> <a href="https://github.com/devwebstudent" target="_blank">GitHub</a></li>
+                    <li><FaSteam /> <a href="#">Steam</a></li>
+                </ul>
             </div>
         </div>
     );
