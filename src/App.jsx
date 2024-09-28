c30b0bdba128a0ef3890e6032e5235599df6656c
devwebstudent <142200863+devwebstudent@users.noreply.github.com>
2024-09-28 01:02:27 -0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
+// Componentes
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 // Pacote chuva
 import { Rainify } from "rainify"
 
@@ -17,40 +21,44 @@ function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <div className="App">
+        <>
             <Rainify isRaining wind={-2} speed={5} thickness={2} intensity={100} splashDuration={8} color="rgba(255,255,255,0.2)" />
-            <h1>Em construção.</h1>
+            
+            <Header />
 
-            <div className="state">
-                <h2>Apenas para testar useState</h2>
-                <p>Clicou: {count}</p>
-
-                <div className="control">
-                    <button onClick={() => setCount(count - 1)}>
-                        <FiMinus className="menos" />
-                    </button>
-
-                    <button onClick={() => setCount(count + 1)}>
-                        <FiPlus className="mais" />
+            <main className="container">
+                <h1>Em construção.</h1>
+                <div className="state">
+                    <h2>Apenas para testar useState</h2>
+                    <p>Clicou: {count}</p>
+                    <div className="control">
+                        <button onClick={() => setCount(count - 1)}>
+                            <FiMinus className="menos" />
+                        </button>
+                        <button onClick={() => setCount(count + 1)}>
+                            <FiPlus className="mais" />
+                        </button>
+                    </div>
+                    <div className="control">
+                    <button onClick={() => setCount(0)}>
+                        <RxReset />
                     </button>
                 </div>
+                </div>
 
-                <div className="control">
-                <button onClick={() => setCount(0)}>
-                    <RxReset />
-                </button>
-            </div>
-            </div>            
+                <div className="socials">
+                    <h3>Onde me encontrar:</h3>
+                    <ul>
+                        <li><FaGithub /> <a href="https://github.com/devwebstudent" target="_blank">GitHub</a></li>
+                        <li><FaSteam /> <a href="#">Steam</a>
+                        </li>
+                    </ul>
+                </div>
 
-            <div className="socials">
-                <h3>Onde me encontrar:</h3>
-                <ul>
-                    <li><FaGithub /> <a href="https://github.com/devwebstudent" target="_blank">GitHub</a></li>
-                    <li><FaSteam /> <a href="#">Steam</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+            </main>
+
+            <Footer/>
+        </>
     );
 }
 
